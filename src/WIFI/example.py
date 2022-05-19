57541d0fd3c63fff3072a2124b110aa47bde10d4
xiaob <a514123661@sina.com>
2022-05-20 01:11:54 +0800

    修改客户端编写的bug

--- src/WIFI/example.py
+++ src/WIFI/example.py
@@ -2,6 +2,6 @@ import socket_tool
 import time
 
 if __name__ == '__main__':
-    service = socket_tool.Service(ip="127.0.0.1", port=8000, pw="codedir")
+    service = socket_tool.Service(ip="127.0.0.1", port=8000)
     socket_tool.rev_message(service)
     
--- src/WIFI/socket_tool.py
+++ src/WIFI/socket_tool.py
@@ -3,21 +3,22 @@ import socket
 
 
 class Client:
-    def __init__(self, ip="127.0.0.0", port=8000, pw=None):
+    def __init__(self, ip="127.0.0.1", port=8000, pw=None):
         self.sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print(ip, port)
-        self.client = self.sk.connect((ip, port))
+        self.sk.connect((ip, port))
 
     def send_msg(self, msg: str):
-        self.client.send(msg.encode("utf-8"))
-        a = self.client.recv(1024)
+        self.sk.send(msg.encode("utf-8"))
+        a = self.sk.recv(1024)
+        a = a.decode("utf-8")
+        print(a)
         if a.endswith("ack OK"):
             return a
         else:
             return "Wrong message"
 
     def rev_message(self):
-        a= self.client.recv(1024)
+        a= self.sk.recv(1024)
         a = a.decode("utf-8")
         return a
 
@@ -31,9 +32,9 @@ class Service:
         self.sk.listen(second)
 
     def rev_message(self):
-        msg, b =  self.sk.accept()
+        msg, b = self.sk.accept()
         a = msg.recv(1024)
-        print(a.decode("utf-8"))
+        print(a)
 
 
 def send_message(client: Client, message=None):
